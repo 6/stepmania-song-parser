a777749d1451ee12bac7ebe7faf94416c330c143
Peter Graham <peterghm@gmail.com>
2013-12-30 06:58:27 -0800

    BackgroundChange class

--- src/BeatMetadata.ts
+++ src/BeatMetadata.ts
@@ -32,4 +32,20 @@ module SmParser {
       return !isNaN(this.beat) && !isNaN(this.value) && this.beat >= 0 && this.value >= 0;
     }
   }
+
+  export class BackgroundChange implements IBeatMetadata {
+    helpers: IHelpers;
+    beat: number;
+    value: string;
+
+    constructor(beat: string, backgroundName: string) {
+      this.helpers = new SmParser.Helpers();
+      this.beat = parseFloat(beat);
+      this.value = backgroundName;
+    }
+
+    isValid() {
+      return !isNaN(this.beat) && this.beat >= 0 && this.helpers.isPresent(this.value);
+    }
+  }
 }
