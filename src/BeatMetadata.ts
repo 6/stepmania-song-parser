7c1003fd29c8b403e81875f4c00e52d179d0cded
Peter Graham <peterghm@gmail.com>
2013-12-30 06:49:56 -0800

    BPM class

---
+++ src/BeatMetadata.ts
@@ -0,0 +1,21 @@
+module SmParser {
+  export interface IBeatMetadata {
+    beat: number;
+    value: any;
+    isValid(): boolean;
+  }
+
+  export class Bpm implements IBeatMetadata {
+    beat: number;
+    value: number;
+
+    constructor(beat: string, bpm: string) {
+      this.beat = parseFloat(beat);
+      this.value = parseFloat(bpm);
+    }
+
+    isValid() {
+      return !isNaN(this.beat) && !isNaN(this.value) && this.beat >= 0 && this.value > 0;
+    }
+  }
+}
