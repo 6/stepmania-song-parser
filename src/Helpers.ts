00f8ba23c0aee5a98c0e7c6dcd748c963bd89419
Peter Graham <peterghm@gmail.com>
2013-12-30 09:39:36 -0500

    presence helpers

--- src/Helpers.ts
+++ src/Helpers.ts
@@ -1,6 +1,8 @@
 module SmParser {
   export interface IHelpers {
     trim(value: string): string;
+    presence(value: any): any;
+    isPresent(value: any): boolean;
   }
 
   // TODO - make this static class
@@ -8,6 +10,17 @@ module SmParser {
     trim(value: string) {
       if(!value) return "";
       return String(value).replace(/^\s+|\s+$/g, "");
+    }
+
+    presence(value: any) {
+      if (value === null) return;
+      if (typeof value === "undefined") return;
+      if (String(value).match(/^\s*$/)) return;
+      return value;
+    }
+
+    isPresent(value: any) {
+      return typeof this.presence(value) !== "undefined";
     }
   }
 }
--- test/helpers_test.js
+++ test/helpers_test.js
@@ -18,4 +18,34 @@ describe("Helpers", function() {
       expect(subject.trim(false)).toEqual("");
     });
   });
+
+  describe("#presence", function() {
+    it("returns the value if present", function() {
+      expect(subject.presence("abc")).toEqual("abc");
+      expect(subject.presence(false)).toEqual(false);
+      expect(subject.presence(123.45)).toEqual(123.45);
+    });
+
+    it("returns `undefined` if not present", function() {
+      expect(subject.presence("")).toBe(undefined);
+      expect(subject.presence("   ")).toBe(undefined);
+      expect(subject.presence(null)).toBe(undefined);
+      expect(subject.presence(undefined)).toBe(undefined);
+    });
+  });
+
+  describe("#isPresent", function() {
+    it("returns true if present", function() {
+      expect(subject.isPresent("abc")).toBe(true);
+      expect(subject.isPresent(false)).toBe(true);
+      expect(subject.isPresent(123.45)).toBe(true);
+    });
+
+    it("returns false if not present", function() {
+      expect(subject.isPresent("")).toBe(false);
+      expect(subject.isPresent("   ")).toBe(false);
+      expect(subject.isPresent(null)).toBe(false);
+      expect(subject.isPresent(undefined)).toBe(false);
+    });
+  });
 });
