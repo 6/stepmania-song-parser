8396876bfb798c7722cb50a3509de530bc90588e
Peter Graham <peterghm@gmail.com>
2014-01-06 23:57:04 -0800

    parse individual note characters

---
+++ src/Note.ts
@@ -0,0 +1,29 @@
+module SmParser {
+  export interface INote {
+    type: string;
+    isValid(): boolean;
+  }
+
+  export class Note implements INote {
+    NoteTypes = {
+      "0": "NoNote",
+      "1": "TapNote",
+      "2": "HoldBeginNote",
+      "3": "HoldEndNote",
+      "4": "RollBeginNote",
+      "M": "Mine",
+      "L": "Lift",
+      "F": "Fake"
+    }
+
+    type: string;
+
+    constructor(public data: string) {
+      this.type = this.NoteTypes[data.toLocaleUpperCase()];
+    }
+
+    isValid() {
+      return typeof this.type !== "undefined";
+    }
+  }
+}
